66fc22a683a6de45c7954b10cd58189a27172408
Asma Saedhiya <asmatsi1806@gmail.com>
2026-07-01 14:00:05 +0530

    Updated React Projects

--- React Projects/src/component/truck.js
+++
@@ -1,7 +0,0 @@
-const trucks =  [
-    {name : "Ashok Leyland", url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTqHsz4Gw1y_LSTu964XUyxkqI-z9RVV0WMJu9R2yV0LRvgmqel", color: "Beige", price : "62L"},
-    {name : "Tata Trucks", url : "https://assets.tractorjunction.com/truck-junction/assets/images/truck/lpt-1916-1694670752.webp", color: "Orange", price : "78L"},
-    {name : "Mahindra Trucks", url : "https://cdn.trucksfloor.com/vehicles/truck/trf/mahindra-furio-7-hd-cargo-2770/mahindra-furio-7-hd-cargo-1.jpg", color: "Red", price : "45L"}
-]
-
-export default trucks
